722418e4613337e0cbfd5a0c2c59474d8ff50e84
Ruben Below <rbelow@hagafu.com>
2017-05-10 01:04:49 +0200

    package

--- web.js
+++ web.js
@@ -1,5 +1,21 @@
 var express = require('express');
 var app = express();
 
+// app.use(express.static(__dirname + '/dist'));
+// app.listen(process.env.PORT || 3000);
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 3000);
+
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+//
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
